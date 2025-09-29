381b6774a6b28e52eda7df5de0e9397ba9c1e9bb
vladimir-v-bvn <Vladimir.V.Bvn@gmail.com>
2025-09-29 17:25:35 +0300

    Update create-kind-cluster.ps1

--- k8s/kind/create-kind-cluster.ps1
+++ k8s/kind/create-kind-cluster.ps1
@@ -1,5 +1,2 @@
 kind create cluster --name microservices --config kind-config.yaml
 #Start-Process -FilePath "kind" -ArgumentList "create cluster", "--name microservices", "--config kind-config.yaml"
-#Set kubectl context to "kind-microservices"
-#You can now use your cluster with:
-#kubectl cluster-info --context kind-microservices
